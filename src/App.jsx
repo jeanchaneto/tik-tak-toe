904aeb536e3af9135960c89a4033dd1da2d7c2a3
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-10-04 08:33:28 +0200

    Check winner functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Cell from "./components/Cell";
 
 function App() {
@@ -7,23 +7,53 @@ function App() {
   const [winningMessage, setWinningMessage] = useState(null);
 
   const message = "it is now " + go + " 's go.";
-console.log(cells)
+  console.log(cells);
+
+  const checkScore = () => {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    winningCombos.forEach((array) => {
+      let circleWins = array.every((cell) => cells[cell] === "circle");
+      if (circleWins) {
+        setWinningMessage("Circle wins");
+      }
+
+      let crossWins = array.every((cell) => cells[cell] === "cross");
+      if (crossWins) {
+        setWinningMessage("Cross wins");
+      }
+    });
+  };
+
+  useEffect(() => {
+    checkScore();
+  }, [cells]);
+
   return (
     <div className="app">
       <div className="gameboard">
         {cells.map((cell, index) => (
-          <Cell 
-          key={index} 
-          id={index} 
-          cell={cell} 
-          setCells={setCells}
-          go={go}
-          setGo={setGo}
-          cells={cells} />
+          <Cell
+            key={index}
+            id={index}
+            cell={cell}
+            setCells={setCells}
+            go={go}
+            setGo={setGo}
+            cells={cells}
+            winningMessage={winningMessage}
+          />
         ))}
       </div>
-
-      <p>{message}</p>
+      <p>{winningMessage || message}</p>
     </div>
   );
 }
--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,33 +1,33 @@
-const Cell = ({ id, cells, cell, setCells, go, setGo }) => {
+const Cell = ({ id, cells, cell, setCells, go, setGo, winningMessage }) => {
   const handleCLick = (e) => {
     const taken =
       e.target.firstChild.classList.contains("circle") ||
       e.target.firstChild.classList.contains("cross");
-
+    if (winningMessage) return;
     if (!taken) {
       if (go === "circle") {
         e.target.firstChild.classList.add("circle");
-        handleCellChange("circle")
+        handleCellChange("circle");
         setGo("cross");
       }
       if (go === "cross") {
         e.target.firstChild.classList.add("cross");
-        handleCellChange("cross")
+        handleCellChange("cross");
         setGo("circle");
       }
     }
   };
 
   const handleCellChange = (className) => {
-const nextCells = cells.map((cell, index) => {
-    if (index === id ) {
-       return className 
-    } else {
-        return cell
-    }
-})
-setCells(nextCells)
-  }
+    const nextCells = cells.map((cell, index) => {
+      if (index === id) {
+        return className;
+      } else {
+        return cell;
+      }
+    });
+    setCells(nextCells);
+  };
 
   return (
     <div className="square" id={id} onClick={handleCLick}>
